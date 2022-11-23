b34890b61c1df1e38fe3c45dff857a423f44ce05
Novitskiy Oleg <onovitskiy29@yandex.ru>
2022-11-24 01:37:18 +0300

    fix2

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,23 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupSave = document.querySelector('.popup__save');
 
+const hartButton = document.querySelector('.card__hart-button');
+
+const onHart = function(){
+    hartButton.classList.add('card__hart-button_active');
+}
+
+const activateHartButton = function(eve) {
+    if (eve.target.classList.contains('card__hart-button_active')){
+        eve.target.classList.remove('card__hart-button_active');
+        return;
+    }
+    eve.target.classList.add('card__hart-button_active');
+}
+
+hartButton.addEventListener('click', activateHartButton);
+
+
 const openPopup = function() {
     popupElement.classList.add('popup_opened');
 }
@@ -11,7 +28,6 @@ const closePopup = function(event) {
 }
 
 const closePopupByClickOnOverlay = function(event) {
-    console.log(event.target, event.currentTarget);
     if (event.target !== event.currentTarget) {
         return;
     } 
@@ -41,3 +57,6 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
+
+nameInput.value = newUserName.textContent;
+jobInput.value = newUserJob.textContent;
